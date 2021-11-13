8c36c6002e970020770e99ab9d22a5a9f7543e57
Rituraj Gupta <rituraj.iter@gmail.com>
2021-11-13 23:18:44 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -107,4 +107,14 @@
     
 })(jQuery);
 
-    
+const count = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/cwc-official.netlify.app/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		count.innerHTML = res.value;
+	})
+}
